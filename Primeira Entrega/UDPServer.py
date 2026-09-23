69adb8a72f71f010e96b9704cd69934d64992f0d
DianneK13 <mclarar.almeida@gmail.com>
2026-09-23 12:35:53 -0300

    feat: implment UDP socket server

---
+++ Primeira Entrega/UDPServer.py
@@ -0,0 +1,28 @@
+#
+#  UDPServer.py
+#  Primeira Entrega
+#
+#  Created by Maria Clara Rodrigues de Almeida on 23/09/26.
+#
+
+from socket import *
+
+# Cria socket UDP
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+# Atribui endereço IP e número de porta ao socket
+serverSocket.bind(('', serverPort))
+
+print("O servidor está a postos! Pronto para receber!")
+
+while True:
+    # Recebe o pacote do cliente junto com o endereço 
+    # de onde ele está vindo.
+    message, clientAddress = serverSocket.recvfrom(2048) # Por que bufsize = 2048? # 
+                                                         # Já que os pacotes que o servidor vai
+                                                         # receber têm 1024 bytes, melhor garantir
+                                                         # com o dobro para não perder nada
+
+    # Abaixo devemos implementadar a lógica de 
+    # reconstrução, armazenamento e devolução 
+    # do arquivo para o cliente
