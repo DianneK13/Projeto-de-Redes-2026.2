adf27a9eb9a612362c4548d58e41bec3eb2c3e78
vThor <vitinho.nascimento07@gmail.com>
2026-09-24 22:15:56 -0300

    envio de txt completo entre cliente e servidor, com fragmentacao

--- Primeira Entrega/UDPClient.py
+++ Primeira Entrega/UDPClient.py
@@ -12,27 +12,47 @@ serverName = "localhost"
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.settimeout(10)
-
 ## ACIMA já temos o socket UDP do cliente implementado ##
 
 ## ABAIXO temos um teste de comunicação ##
 
 # Posteriormente implementaremos a lógica de 
-# reconstrução e armazenamento 
+# reconstrução e armazenamento  
 # do arquivo recebido do servidor
 
 # Mensagem
 message = "Quer hamburguer?"
+tamanho_chunk = 1024
+
+
 
 # Enviando a mensagem e aguardando a resposta
-clientSocket.sendto(message.encode(),(serverName, int(serverPort)))
-print("client_ " + message)
+
+with open("arquivo.txt", 'rb') as arquivo: #abro o arquivo da mensagem que vai ser transmitida
+    while True: 
+        data = arquivo.read(tamanho_chunk) #leio até o tamanho bater o 1024 bytes
+        if not data:
+            break
+        clientSocket.sendto(data, (serverName, int(serverPort)))
+    clientSocket.sendto(b"", (serverName, int(serverPort))) #pacote vazio para reconhecer o fim da transmissão
 
 # Cliente recebe uma resposta do servidor
-encodedModified = clientSocket.recv(2048)
 
-modifiedMessage = encodedModified.decode()
-print("server_ " + modifiedMessage)
+    with open("cliente_recebe.txt", 'ab') as output: # abro o arquivo que recebe a mensagem que volta do servidor
+        while True:
+            encodedModified = clientSocket.recv(2048) 
+            # Por que bufsize = 2048? # 
+            # Já que os pacotes que o servidor vai
+            # receber têm 1024 bytes, melhor garantir
+            # com o dobro para não perder nada(BUF_SIZE)
+            if not encodedModified: #recebe um pacote vazio, que representa o fim da mensagem
+                break
+            print("segmentou\n")
+            output.write(encodedModified) # escrevo o pacote recebido do servidor no arquivo
+
+
+#modifiedMessage = encodedModified.decode()
+#print("server_ " + modifiedMessage)
 print("client_ Compreensível. Tenha um bom dia!")
 clientSocket.close()
 print("client_ Conexão fechada.")
--- Primeira Entrega/UDPServer.py
+++ Primeira Entrega/UDPServer.py
@@ -12,25 +12,39 @@ serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 # Atribui endereço IP e número de porta ao socket
 serverSocket.bind(('', serverPort))
+tamanho_chunk = 1024
+
 
 print("O servidor está a postos! Pronto para receber!")
 
 while True:
     # Recebe o pacote do cliente junto com o endereço 
     # de onde ele está vindo.
-    message, clientAddress = serverSocket.recvfrom(2048) # Por que bufsize = 2048? # 
-                                                         # Já que os pacotes que o servidor vai
-                                                         # receber têm 1024 bytes, melhor garantir
-                                                         # com o dobro para não perder nada
+    with open("download.txt", 'ab') as output: #abro o arquivo que vai salvar a mensagem transmitida pelo servidor
+        while True:
+            message, clientAddress = serverSocket.recvfrom(2048) # Por que bufsize = 2048? # 
+                                                                 # Já que os pacotes que o servidor vai
+                                                                 # receber têm 1024 bytes, melhor garantir
+                                                                 # com o dobro para não perder nada(BUF_SIZE)
+            if not message: #recebe o pacote vazio
+                break
+            print("Segmentou\n")
+            output.write(message)
+
+    with open("arquivo.txt", 'rb') as arquivo: #mesma lógica da parte do cliente para enviar
+        while True:
+            data = arquivo.read(tamanho_chunk)
+            if not data:
+                break
+            serverSocket.sendto(data, clientAddress)
+        serverSocket.sendto(b"", clientAddress)
 
     # Abaixo devemos implementadar a lógica de 
     # reconstrução, armazenamento e devolução 
     # do arquivo para o cliente
 
     # TESTE DE COMUNICAÇÃO FUNCIONANDO #
-    modifiedMessage = message.decode()
-    print("client_ " + modifiedMessage)
+    #modifiedMessage = message.decode()
+    #print("client_ " + modifiedMessage)
 
-    response = "Quero não"
-    print("server_ " + response)
-    serverSocket.sendto(response.encode(), clientAddress)
+    print("server_mensagem recebida" )
